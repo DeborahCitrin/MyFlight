2e68e18d67ade2e2fe1dcc697ce343d44b7ead6d
Deborah <dcitrinf@gmail.com>
2022-08-29 12:19:47 -0300

    adição do método distancia() à classe Geo

--- Teste/src/Geo.java
+++ Teste/src/Geo.java
@@ -1,3 +1,5 @@
+import java.lang.Math;
+
 public class Geo {
 	private double latitude;
 	private double longitude;
@@ -14,4 +16,23 @@ public class Geo {
 	public double getLongitude() {
 		return longitude;
 	}
+
+	public static double distancia(Geo a, Geo b)
+	{
+		double senLat = Math.sin((a.getLatitude()-b.getLatitude())/2);
+		senLat *= senLat;
+
+		double senLong = Math.sin((a.getLongitude() - b.getLongitude())/2);
+		senLong *= senLong;
+
+		double vezes = senLong * Math.cos(a.getLatitude()) * Math.cos(b.getLatitude());
+
+		double soma = senLat + vezes;
+		soma = Math.sqrt(soma);
+
+		return 2 * 6371 * (Math.asin(Math.sqrt(soma)));
+	}
+
+	//Ver o exercício 2 do Sobregarga... 
+	//Dados Armazenados??
 }
