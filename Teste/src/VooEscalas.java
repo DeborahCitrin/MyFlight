eb53a4d310502dd96a0ff6361f6c6e95b7e0542b
Deborah <dcitrinf@gmail.com>
2022-09-05 12:37:13 -0300

    Adição da classe VooEscalas

---
+++ Teste/src/VooEscalas.java
@@ -0,0 +1,24 @@
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class VooEscalas extends Voo 
+{
+    private Rota rotaFinal;
+
+    public VooEscalas(Rota rota, Rota rotaFinal, LocalDateTime datahora, Duration duracao)
+    {
+        super(rota, datahora,duracao);
+        this.rotaFinal = rotaFinal;
+    }
+
+    public Rota getRotaFinal()
+    {
+        return rotaFinal;
+    }
+    
+    @Override
+    public String toString()
+    {
+        return super.toString() + " -> " + rotaFinal;
+    }
+}
